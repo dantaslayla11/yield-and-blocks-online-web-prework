9082f43cd77dc8d0103eee175cfe32a9507c8bce
layla dantas <laylatdantas@gmail.com>
2019-05-22 22:22:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,24 +1,3 @@
-def hello_t(array)
-  puts array
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
 
-
-hello_t(["fuck","fucky"]) do |name|
-  if name.start_with?("f")
-    puts "Hi, #{name}"
-  end
-end
 
 
