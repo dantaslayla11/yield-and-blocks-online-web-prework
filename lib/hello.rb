70100dcecb50343d9edb637c8d18f64aeeadba16
layla dantas <laylatdantas@gmail.com>
2019-05-22 22:15:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,3 @@
-def hello_t(array)
-  puts array
-  i = 0
- 
-  while i < array.length
-    puts array[i]
-    #yield array[i]
-    i = i + 1
-  end
-  puts i
-end
 
 
 hello_t(["fuck","fucky"]) do |name|
